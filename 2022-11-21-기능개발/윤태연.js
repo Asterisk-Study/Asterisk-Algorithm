d2f588cc044767a83b24cd8af864badc73a70763
rhino-ty <okko8522@gmail.com>
2022-11-18 23:53:11 +0900

    docs(기능개발/윤태연.md): 알고리즘 구현 중

---
+++ 2022-11-21-기능개발/윤태연.js
@@ -0,0 +1,23 @@
+function solution(progresses, speeds) {
+  // 큐, 새 배열에 끝나는 날을 넣고 그 날을 순회하여 비교하는 식으로?
+  // 1. 작업이 끝난 날짜를 새 배열 넣기
+  let doneQ = [];
+  for (let i = 0; i < progresses.length; i++) {
+    // 작업 날짜 구하기? 100에 빼기
+    doneQ.push(Math.ceil((100 - progresses[i]) / speeds[i]));
+  }
+  // 2. 날짜들을 순회하여 기준(0번째)보다 작다면 카운트++, 크다면 그동안의 카운트를 새 배열로 옮기고 초기화
+  let count = 1;
+  let standard = doneQ[0];
+  let result = [];
+  doneQ.forEach((day) => {
+    if (standard >= day) {
+      count++;
+    } else {
+      result.push(count);
+      standard = day;
+      count = 1;
+    }
+  });
+  return result;
+}
